449e38e37559a88f675309a8d64746e7b054437e
PatrykDrozdz <pdrozdz@onet.eu>
2016-12-03 22:02:55 +0100

    some look changes

--- TaxPayer/CSS_database_connection_tries/Result.Designer.cs
+++ TaxPayer/CSS_database_connection_tries/Result.Designer.cs
@@ -37,36 +37,40 @@
             // taxLabel
             // 
             this.taxLabel.AutoSize = true;
-            this.taxLabel.Location = new System.Drawing.Point(37, 28);
+            this.taxLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.taxLabel.Location = new System.Drawing.Point(35, 26);
             this.taxLabel.Name = "taxLabel";
-            this.taxLabel.Size = new System.Drawing.Size(121, 13);
+            this.taxLabel.Size = new System.Drawing.Size(237, 25);
             this.taxLabel.TabIndex = 0;
             this.taxLabel.Text = "Podatek do zapłacenia:";
             // 
             // taxValueLabel
             // 
             this.taxValueLabel.AutoSize = true;
-            this.taxValueLabel.Location = new System.Drawing.Point(37, 110);
+            this.taxValueLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.taxValueLabel.Location = new System.Drawing.Point(36, 147);
             this.taxValueLabel.Name = "taxValueLabel";
-            this.taxValueLabel.Size = new System.Drawing.Size(95, 13);
+            this.taxValueLabel.Size = new System.Drawing.Size(193, 25);
             this.taxValueLabel.TabIndex = 1;
-            this.taxValueLabel.Text = "Wartość podatku: ";
+            this.taxValueLabel.Text = "Kwota podatkowa: ";
             // 
             // taxValueCount
             // 
             this.taxValueCount.AutoSize = true;
-            this.taxValueCount.Location = new System.Drawing.Point(37, 57);
+            this.taxValueCount.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.taxValueCount.Location = new System.Drawing.Point(36, 67);
             this.taxValueCount.Name = "taxValueCount";
-            this.taxValueCount.Size = new System.Drawing.Size(10, 13);
+            this.taxValueCount.Size = new System.Drawing.Size(15, 20);
             this.taxValueCount.TabIndex = 4;
             this.taxValueCount.Text = "-";
             // 
             // taxValue
             // 
             this.taxValue.AutoSize = true;
-            this.taxValue.Location = new System.Drawing.Point(37, 151);
+            this.taxValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.taxValue.Location = new System.Drawing.Point(36, 188);
             this.taxValue.Name = "taxValue";
-            this.taxValue.Size = new System.Drawing.Size(10, 13);
+            this.taxValue.Size = new System.Drawing.Size(15, 20);
             this.taxValue.TabIndex = 5;
             this.taxValue.Text = "-";
             // 
@@ -74,15 +78,15 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(222, 199);
+            this.ClientSize = new System.Drawing.Size(286, 228);
             this.Controls.Add(this.taxValue);
             this.Controls.Add(this.taxValueCount);
             this.Controls.Add(this.taxValueLabel);
             this.Controls.Add(this.taxLabel);
             this.MaximizeBox = false;
-            this.MaximumSize = new System.Drawing.Size(238, 237);
+            this.MaximumSize = new System.Drawing.Size(302, 266);
             this.MinimizeBox = false;
-            this.MinimumSize = new System.Drawing.Size(238, 237);
+            this.MinimumSize = new System.Drawing.Size(302, 266);
             this.Name = "Result";
             this.Text = "Wynik";
             this.ResumeLayout(false);
